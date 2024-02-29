cb2d6b6e440d9cef24bf943e19d45c6a6e53bb93
prachisonani <lakhaniprachi8@gmail.com>
2024-02-29 22:13:54 +0530

    timer

--- timer/timmer.js
+++ timer/timmer.js
@@ -1,16 +1,15 @@
 const d = new Date();
 
-
+//https://codepen.io/eddyerburgh/pen/yOjdqo
 //time
 let Hours = d.getHours();
-
-document.getElementById("Hours").innerHTML = Hours + ":";
+document.getElementById("Hours").innerHTML = Hours;
 
 let Minutes = d.getMinutes();
-document.getElementById("Minutes").innerHTML = Minutes + ":";
+document.getElementById("Minutes").innerHTML = Minutes;
 
 let Seconds = d.getSeconds();
-document.getElementById("Seconds").innerHTML = Seconds + ":";
+document.getElementById("Seconds").innerHTML = Seconds;
 //ampm
 if (Hours <= 12) {
     document.getElementById("ampm").innerHTML = "am";
@@ -22,13 +21,13 @@ if (Hours <= 12) {
 
 //date
 
-let day = ["sunday", "monday", "tuesdat", "Wensday", "thursday", "friday", "seturday"]
+let day = ["Sunday", "Monday", "Tuesday", "Wednseday", "Thursday", "Friday", "Saturday"]
 
 let a = day[d.getDay()];
 document.getElementById("days").innerHTML = a + ",";
 
 
-let month = ["jan", "feb", "mar", "apr", "may", "june", "july", "aug", "sep", "oct", "nev", "dec"]
+let month = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nev", "Dec"]
 
 let b = month[d.getMonth()];
 document.getElementById("months").innerHTML = b;
@@ -38,3 +37,4 @@ document.getElementById("date").innerHTML = date;
 
 let year = d.getFullYear();
 document.getElementById("year").innerHTML = year;
+
